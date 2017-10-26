8076bd191fa14e2cded1e0ace12235f30203441c
Unknown <robert.corman@tss-yonder.com>
2017-10-26 09:41:26 +0300

    Cats added

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -6,7 +6,28 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Robert Edited!");
+            var cat = new HelloKitty(){
+                Name="Tomas"
+            };
+            var cat1 = new HelloKitty(){
+                Name="Garfield"
+            };
+            var cat2 = new HelloKitty(){
+                Name="Tommy"
+            };
+            cat1.MakeNoise();
+            cat2.MakeNoise();
+            cat.MakeNoise();
         }
+    }
+    public class HelloKitty
+    {
+        public String Name { get; set; }
+
+        public void MakeNoise()
+        {
+            Console.WriteLine(Name + " says Meow!");
+        }
+
     }
 }
